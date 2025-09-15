7ed22e29fb82739d6efd453cff08eaf11f4b6351
ncarlson8 <ncarlson8flcc@gmail.com>
2025-09-15 15:50:24 +0000

    9/15/2025 organizing and work

---
+++ chapter2/dollar_to_rupee.py
@@ -0,0 +1,3 @@
+dollar = float(input('How many USD do you want to convert to Rupees? '))
+con = dollar * 88.15
+print(f'Your ${dollar} convert to {con} Rupees.')
---
+++ chapter2/feet_to_miles.py
@@ -0,0 +1,2 @@
+feet = int(input('Enter the amount of feet: '))
+print(f'{feet} feet is equivalent to {feet // 5280} miles and {feet % 5280} feet.')
