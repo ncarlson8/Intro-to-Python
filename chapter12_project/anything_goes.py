484f2165cdbccb1acdb22cc43e696540cbbb83d5
ncarlson8 <ncarlson8flcc@gmail.com>
2025-12-03 17:18:01 +0000

    12/3

---
+++ chapter12_project/anything_goes.py
@@ -0,0 +1,40 @@
+# FILE NAME - anything_goes.py
+# Remember to cite any sources you used to help you complete this project!
+
+# NAME: Nick Carlson
+# DATE: 12/3/2025
+# BRIEF DESCRIPTION: Final Project
+
+# Library of audiovisual, literary, and musical media. Entries can be added and removed.
+# Menu for choosing one of the three, then another menu to show only a certain genre, favorites, or others
+# can then choose a specific book, song, movie, etc. to bring up a brief description or synopsis.
+
+
+
+
+
+
+
+
+
+
+
+
+########################################
+#          REFLECTION QUESTIONS
+########################################
+
+'''
+Remember to cite any sources you used to help you complete this project.
+You can do that here in this section or cite as comments throughout your code.
+
+1. How did you choose the goal of your program?
+
+
+2. What part of this project challenged you the most? How did you work through that challenge?
+
+
+3. If you had 10 more hours to improve or expand your program, what would you change? Why?
+
+
+'''
